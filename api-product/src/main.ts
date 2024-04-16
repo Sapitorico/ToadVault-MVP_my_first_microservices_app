197aa4abff808160e76478fd2ac9b805192d85bc
sapitorico <renzoyaques@gmail.com>
2024-04-15 21:15:31 -0300

    api verison control

--- api-product/src/main.ts
+++ api-product/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Configurar el prefijo de la API y la versión
+const apiPrefix = 'api';
+const apiVersion = 'v1';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix(`${apiPrefix}/${apiVersion}`);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
