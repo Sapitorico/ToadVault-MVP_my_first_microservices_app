2a52a0ae0deb5a46b560977a1fcf51965c97d673
sapitorico <renzoyaques@gmail.com>
2024-04-20 12:12:37 -0300

    delete api cart

--- api-cart/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { CartModule } from './cart.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(CartModule);
-  await app.listen(3000);
-}
-bootstrap();
