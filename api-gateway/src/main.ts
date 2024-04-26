30aa9be00b87851a9b96aa716f840d891d454992
sapitorico <renzoyaques@gmail.com>
2024-04-26 11:48:33 -0300

    update swagger prefix

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -13,18 +13,17 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('ToadVault')
     .setDescription('ToadVault is a web-based cash register application built on microservices architecture.')
-    .setDescription('The cats API description')
     .setVersion(apiVersion)
     .addBearerAuth()
     .addTag('authentication')
     .addTag('inventory')
     .addTag('order')
     .addTag('payment')
-    .addServer('http://localhost:3000', 'Local environment')
+    .addServer(`http://localhost:3000/${apiPrefix}/${apiVersion}`, 'Local environment')
     .addServer(`https://${domain}`, 'Production')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup(`${apiPrefix}`, app, document);
   await app.listen(port || 3000);
 }
 bootstrap();
